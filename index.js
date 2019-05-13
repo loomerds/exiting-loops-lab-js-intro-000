e6e50a6dfeb0c3ac67ab3df13257681a4cbd1bd7
Doug Loomer <loomerds@gmail.com>
2019-05-13 12:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 function keepGoing(array, changeValue, skipValue) {
-
+  for(let i=0; i<array.length; i++) {
+    if(element===skipValue) {
+      continue;
+    } else {
+      element = changeValue;
+    }
+  }
+  return array;
 }
 
 function findBy(array, findFn) {
